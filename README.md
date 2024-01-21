dc0960c249665b2619c3062445008aaf03e3e8d2
OptimisticUnderdog <mabunda.wealth@gmail.com>
2024-01-21 16:55:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# Weather App
+
+## Overview
+
+This is a straightforward weather app that provides users with basic current weather information for a given city. It allows users to check the temperature and a brief description of the weather conditions.
+
+## Table of Contents
+
+- [Features](#features)
+- [Demo](#demo)
+- [Getting Started](#getting-started)
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Technologies Used](#technologies-used)
+- [Acknowledgements](#acknowledgements)
+- [License](#license)
+
+## Features
+
+- **Current Weather:**
+  - Display the current temperature in Celsius.
+  - Show a brief description of the weather conditions.
+
+- **Location Input:**
+  - Users can enter the name of the city to get weather information.
+
+## Demo
+
+### Screenshots
+
+![Demo 1](assets/images/WeatherScreenshot.JPG)
+*Home Page*
+
+![Demo 2](assets/images/WeatherScreenshot1.JPG)
+*Support Page*
+
+![Demo 3](assets/images/WeatherScreenshot2.JPG)
+*About Page*
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js and npm installed on your machine.
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+    git clone https://github.com/your-username/weather-app.git
+
+2. Navigate to the project directory:
+    cd weather-app
+
+3. Go live:
+    port:5500
+
+## Technologies Used
+
+- HTML/CSS
+- JavaScript
+- Bootstrap
+
+## Acknowledgements
+
+- Weather data sourced from API Ninjas [API Ninjas](https://api-ninjas.com/).
+- Logo obtained from Vectors [Vectors](https://www.vecteezy.com/png/27079477-sun-with-rain-light-weather-3d-illustration).
+- Gif's and background image obtained from Gifer [gifer](https://gifer.com/en/).
+- Illustrations obtained from Icons8 [icons8](https://icons8.com/illustrations).
+- Icons obtained from Google fontawesome icons [google](https://fontawesome.com/).
+- Favicon created from Favicon.io [Favicon](favicon.io).
+
+## License
+
+* This project is licensed under MIT License, see the LICENSE file for details.
---
+++ script.js
@@ -0,0 +1,170 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '5d1150382dmsh8148168445631d9p19de75jsn7af80ac1db5d',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+const getWeatherByLocation = () => {
+    if (navigator.geolocation) {
+        // Get user's current location
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const { latitude, longitude } = position.coords;
+                fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?lat=${latitude}&lon=${longitude}`, options)
+                    .then(response => response.json())
+                    .then(response => {
+                        const city = response.city;
+                        // Update weather based on user's location
+                        getWeather(city);
+                    })
+                    .catch(err => console.error(err));
+            },
+            (error) => {
+                console.error(error.message);
+            }
+        );
+    } else {
+        console.error('Geolocation is not supported by this browser.');
+    }
+}
+
+// Call getWeatherByLocation to prompt user for location and update weather
+getWeatherByLocation();
+
+const getWeather = (city) => {
+    // Show loading spinner
+    document.getElementById('loadingSpinner').classList.remove('d-none');
+
+     // Show loading spinner only if screen width is greater than 768px
+     if (window.innerWidth > 768) {
+        loadingSpinner.classList.remove('d-none');
+    }
+
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then((response) => {
+            cityName.innerHTML = city;
+            console.log(response);
+            
+            // Update current weather information
+            temp.innerHTML = response.temp;
+            temp2.innerHTML = response.temp;
+            feels_like.innerHTML = response.feels_like;
+            humidity.innerHTML = response.humidity;
+            humidity2.innerHTML = response.humidity;
+            min_temp.innerHTML = response.min_temp;
+            max_temp.innerHTML = response.max_temp;
+            wind_speed.innerHTML = response.wind_speed;
+            wind_speed2.innerHTML = response.wind_speed;
+            wind_degrees.innerHTML = response.wind_degrees;
+            sunrise.innerHTML = response.sunrise;
+            sunset.innerHTML = response.sunset;
+
+            // Fetch and update forecast information
+            getForecast(city);
+        })
+        .catch(err => console.error(err))
+        .finally(() => {
+            // Hide loading spinner after data is loaded (or in case of an error)
+            document.getElementById('loadingSpinner').classList.add('d-none');
+        });
+}
+
+window.addEventListener('resize', () => {
+    // Call getWeather when the window is resized
+    getWeather(city.value);
+});
+
+const getForecast = (city) => {
+    // Fetch forecast data
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/forecast?city=' + city, options)
+        .then(response => response.json())
+        .then(forecastResponse => {
+            // Update forecast information
+            updateForecast(forecastResponse);
+        })
+        .catch(err => console.error(err));
+}
+
+const updateForecast = (forecastResponse) => {
+    const forecastList = document.getElementById('forecastList');
+    forecastList.innerHTML = '';
+
+    if (forecastResponse && forecastResponse.forecast) {
+        // Check if forecastResponse and forecast property exist
+        forecastResponse.forecast.forEach(day => {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = `Day: ${day.date}, Temperature: ${day.temperature}&#8451;, Weather: ${day.weather}`;
+            forecastList.appendChild(listItem);
+        });
+    } else {
+        forecastList.innerHTML = 'No forecast data available, try another location.';
+    }
+}
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    getWeather(city.value);
+});
+
+TweenMax.from(".nav-anim" , 0.8 , {
+    delay : 0,
+    opacity : 0,
+    y : -20,
+    ease: Expo.easeInOut
+})
+
+
+TweenMax.from(".header-anim" , 0.9 , {
+    delay : 0,
+    opacity : 0,
+    y : -20,
+    ease: Expo.easeInOut
+})
+
+
+TweenMax.from(".card-1" , 0.9 , {
+    delay : 0.3,
+    opacity : 0,
+    y : 20,
+    ease: Expo.easeInOut
+})
+
+
+TweenMax.from(".card-2" , 0.9 , {
+    delay : 0.5,
+    opacity : 0,
+    y : 40,
+    ease: Expo.easeInOut
+})
+
+
+TweenMax.from(".card-3" , 0.9 , {
+    delay : 0.7,
+    opacity : 0,
+    y : 60,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".card-4" , 0.9 , {
+    delay : 0.9,
+    opacity : 0,
+    y : 60,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".card-5" , 0.9 , {
+    delay : 0.11,
+    opacity : 0,
+    y : 60,
+    ease: Expo.easeInOut
+})
+
+TweenMax.from(".name-anim" , 0.9 , {
+    delay : 0.7,
+    opacity : 0,
+    y : 60,
+    ease: Expo.easeInOut
+})
